fec8939cedbf4410b02d4b3e2b9436bc5e08c180
CSA100 <chaanan100@gmail.com>
2021-03-29 12:11:29 +0800

    Updates

---
+++ precache-manifest.a58be5e62d2c281d09153b042df302b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac156ae45a31e4e1fd73b60893f23c9e",
+    "url": "/teslaclone/index.html"
+  },
+  {
+    "revision": "aa53375ee5db77780c02",
+    "url": "/teslaclone/static/css/main.9f00f89e.chunk.css"
+  },
+  {
+    "revision": "b56700dc78ede8837039",
+    "url": "/teslaclone/static/js/2.f224a8aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/teslaclone/static/js/2.f224a8aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa53375ee5db77780c02",
+    "url": "/teslaclone/static/js/main.704f02cf.chunk.js"
+  },
+  {
+    "revision": "f2423a30a9fd5dd2a47b",
+    "url": "/teslaclone/static/js/runtime-main.561fe015.js"
+  },
+  {
+    "revision": "e19c20e966bde501f94e41cd0322dbe8",
+    "url": "/teslaclone/static/media/Gotham Medium.e19c20e9.otf"
+  },
+  {
+    "revision": "7261d83c93d6ae9cb305badf6f6ed43b",
+    "url": "/teslaclone/static/media/hiclipart.com.7261d83c.png"
+  }
+]);
